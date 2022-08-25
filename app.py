47864bd4ac88b3650b1f8c1e1c6a1e02399717e7
Subdiduntil <manosserlis@gmail.com>
2022-08-25 15:45:22 +0300

    footer-navbar-font

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from flask import Flask, render_template
 from flask_frozen import Freezer
 from livereload import Server
 
-template_folder = path.abspath('./wiki')
+#template_folder = path.abspath('./wiki')
 
-app = Flask(__name__, template_folder=template_folder)
+app = Flask(__name__, template_folder='wiki')
 #app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
 app.config['FREEZER_DESTINATION'] = 'public'
 app.config['FREEZER_RELATIVE_URLS'] = True
@@ -29,7 +29,8 @@ def index():
 
 @app.route('/<page>')
 def pages(page):
-    return render_template(str(Path('pages') / (page.lower() + '.html')))
+     return render_template(str(Path('pages')) + '/' + page.lower() + '.html')
+
 
 # Main Function, Runs at http://0.0.0.0:8080
 if __name__ == "__main__":
@@ -39,5 +40,3 @@ if __name__ == "__main__":
         Server(app.wsgi_app).serve()
     else:
         app.run(port=8080)
-
-   
